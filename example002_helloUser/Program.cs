c916478f5bc6176b3b1cf25f728d78fcea0fdde5
Nastia <titova_anastasia@tut.by>
2023-02-21 23:42:53 +0300

    example003_sum

---
+++ example002_helloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Write you name");
+string username = Console.ReadLine();
+Console.WriteLine("Hello,");
+Console.WriteLine(username);
---
+++ example003_sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numbera = 22;
+int numberb = 5;
+int result = numbera + numberb;
+Console.WriteLine(result);
