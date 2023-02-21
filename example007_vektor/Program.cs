cf8ccc92fff475ca2c3a1c101dc1a9db69f6c9e6
Nastia <titova_anastasia@tut.by>
2023-02-22 01:05:54 +0300

    example007_vektor

---
+++ example007_vektor/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(1,3);
+//Console.Write("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.Write("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.Write("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.Write("+");
+
+int x = xa, y = ya;
+
+int count = 0;
+
+while (count<10000)
+{
+    int what = new Random().Next(0,3);
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+
+}
