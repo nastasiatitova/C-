22f2f47975aecec688b4878fc31cd3338de4ac3a
Nastia <titova_anastasia@tut.by>
2023-02-22 00:24:20 +0300

    example005_helloMasha

---
+++ example005_helloMasha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Whot is you name");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow, this is Masha!");
+}
+else
+{
+    Console.WriteLine("Hello");
+    Console.WriteLine(username);
+}
