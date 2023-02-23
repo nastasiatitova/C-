404ab2a06cd2eec02cac46b718504fed545e2b91
Nastia <titova_anastasia@tut.by>
2023-02-23 11:08:30 +0300

    Zadacha s kvadratom

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿//4-16
+//-3=9
+//
+Console.WriteLine("Number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1*number1 == number2)
+{
+    Console.WriteLine("Verno");
+}
+else Console.WriteLine("Ne verno");
