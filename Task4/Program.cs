5a7203b1c0ad82d811ca63a6773bc380cb3c105a
Nastia <titova_anastasia@tut.by>
2023-02-23 12:16:14 +0300

    Zadacha s vivodom

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Number");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+Console.WriteLine("Vivod chisel");
+while (count<=number)
+{
+   Console.Write(" " + count);
+   count = count+1;
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿// ввод 3зн число, вывод последнюю цифру
+Console.WriteLine("Number");
+int number = Convert.ToInt32(Console.ReadLine());
+int div = 0;
+if (number>=100 && number<1000)
+{
+    div = number % 10;
+    Console.WriteLine(div);
+}
+else
+{
+Console.WriteLine("Ne tot Number");
+}
