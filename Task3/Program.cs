ab24d6743c2e2831edf51f397f4cb4cae59de2b2
Nastia <titova_anastasia@tut.by>
2023-02-23 11:24:08 +0300

    Zadacha s dniami

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Number");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+    case (3):
+    Console.WriteLine("Sreda");
+    break;
+    case (1):
+    Console.WriteLine("Poned");
+    break;
+    case (2):
+    Console.WriteLine("Vtornik");
+    break;
+    case (4):
+    Console.WriteLine("Chetv");
+    break;
+    case (5):
+    Console.WriteLine("Piatnica");
+    break;
+    case (6):
+    Console.WriteLine("Ssybota");
+    break;
+     case (7):
+    Console.WriteLine("Voskr");
+    break;
+    default: 
+    Console.WriteLine("Ne vernoe hcislo");
+    break;
+}
