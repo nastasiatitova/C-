1b85623848906ab70de3aa8fa284b902437cfaf1
Nastia <titova_anastasia@tut.by>
2023-02-27 10:47:51 +0300

    Sem2/2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Random rand = new Random();
+int number = rand.Next(100,1000);
+Console.WriteLine("Случайное число это - " + number);
+
+int num1 = number / 100;
+int num2 = number % 10;
+Console.Write(num1);
+Console.WriteLine(num2);
