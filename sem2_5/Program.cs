cf579fc59c7ec4f60956de27304e5a6823f109c1
Nastia <titova_anastasia@tut.by>
2023-02-27 11:10:22 +0300

    Sem2/5

---
+++ sem2_5/Program.cs
@@ -0,0 +1,10 @@
+﻿// число, проверка кратности 7 и 23
+
+Console.WriteLine("Number1");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 || number % 23 == 0)
+
+   Console.WriteLine("да"); 
+
+else Console.WriteLine("нет");
