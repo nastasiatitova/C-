c12b3ed0e789f67d273c59631ae648d68c50db42
Nastia <titova_anastasia@tut.by>
2023-02-27 10:49:04 +0300

    Sem2/1

---
+++ Semin2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное 
+// число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+Random rand = new Random();
+int number = rand.Next(10,100);
+Console.WriteLine("Случайное число это - " + number);
+int max = number / 10;
+int min = number % 10;
+
+if (max>min)
+{
+    Console.WriteLine("Наибольшая цифра числа это {0}", max);
+}
+else
+Console.WriteLine("Наибольшая цифра числа это {0}", min);
+
