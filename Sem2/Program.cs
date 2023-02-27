bce188e80e01198f7f7c24460fbcac3719639a79
Nastia <titova_anastasia@tut.by>
2023-02-27 10:49:56 +0300

    Sem2/3

---
+++ Sem2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2 || number2 == number1*number1)
+
+   Console.WriteLine("да"); 
+
+else Console.WriteLine("нет");
