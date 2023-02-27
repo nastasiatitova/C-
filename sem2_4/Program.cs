d473f7728105ce180108f4abf84815cdd8b54880
Nastia <titova_anastasia@tut.by>
2023-02-27 11:15:39 +0300

    Sem2/4

---
+++ sem2_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2 числа на вход, проверить кратность
+
+Console.WriteLine("Number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % number2 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.Write("не кратно, остаток ");
+    Console.WriteLine(number1 % number2);
+}
