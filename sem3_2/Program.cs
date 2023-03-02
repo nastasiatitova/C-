1cd85f68bdc02b0ab86541b205b66cca0a643596
Nastia <titova_anastasia@tut.by>
2023-03-02 10:43:05 +0300

    sem3_2

--- sem3_2/Program.cs
+++ sem3_2/Program.cs
@@ -1,17 +1,18 @@
 ﻿//Напишите программу, которая по заданному номеру 
 //четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
-Console.WriteLine("Введите номер четверти");
+Console.WriteLine("Введите номер четверти: от 1 до 4");
 int number = Convert.ToInt32(Console.ReadLine());
 
 
 if (number == 1)
-Console.WriteLine("x>0 && Y>0");
+Console.WriteLine("x > 0 и Y > 0");
 else if (number == 2)
-Console.WriteLine("x<0 && Y>0");
+Console.WriteLine("x < 0 и Y > 0");
 else if (number == 3)
-Console.WriteLine("x<0 && Y<0");
+Console.WriteLine("x < 0 и Y < 0");
 else if (number == 4)
-Console.WriteLine("x>0 и Y>0");
-else Console.WriteLine("Введено тнекорректный номер");
+Console.WriteLine("x > 0 и Y > 0");
+else Console.WriteLine("Введено текорректный номер");
 
+// можно решать через switch (number)
