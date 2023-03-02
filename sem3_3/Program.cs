08a2533ad9fdcdb9c78e44bd523ca4a708518406
Nastia <titova_anastasia@tut.by>
2023-03-02 10:35:07 +0300

    sem3_3

---
+++ sem3_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// вывести квадраты чисел от 1 до n
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+
+    Console.Write(i*i + " ");
