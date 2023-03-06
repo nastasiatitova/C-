901d9bfa6db106fcc8c304b174967b19e8b79c12
Nastia <titova_anastasia@tut.by>
2023-03-06 11:49:58 +0300

    sem4_3

---
+++ sem4_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// массив из 8 эл-в  -  0 1 в случ порядке
+
+int Prompt()
+{
+Console.WriteLine("Введите количество элементов");
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray(int size) 
+{
+int [] array = new int [size];
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(0,2);
+}
+return array;
+} 
+
+int[] newArray = CreateArray(Prompt());
+
+Console.WriteLine($"[{String.Join(", " , newArray)}]");
