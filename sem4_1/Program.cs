9f08225e51bb8b8f81407da689fb2375b3b5a6ac
Nastia <titova_anastasia@tut.by>
2023-03-06 11:51:13 +0300

    sem4_1

---
+++ sem4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int Prompt()
+{
+Console.WriteLine("Введите число");
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNum(int number)
+{
+int sum = 0;
+for (int i = 1; i <= number; i++);
+   sum = sum + i;
+return sum;
+}
+
+int a = Prompt();
+Console.WriteLine(SumNum(a));
