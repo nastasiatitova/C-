dd1974fbbeabd5b3a5e58c3f917c7be8f11bca7f
Nastia <titova_anastasia@tut.by>
2023-03-14 12:06:17 +0300

    lekcia4

---
+++ Lekcia4/Program.cs
@@ -0,0 +1,148 @@
+﻿/*
+string[,] table = new string[2,5];
+
+table[1,2] = "слово";
+
+ for (int rows = 0; rows < 2; rows++)
+ {
+    for (int columns = 0; columns < 5; columns++)
+    {
+       Console.WriteLine($"-{table[rows, columns]}-");
+    }
+ }
+*/
+/*
+ int[,] matrix = new int[3,4];
+ for (int i = 0; i < matrix.GetLength(0); i++)
+ {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       Console.Write($"{matrix[i, j]} ");
+    }
+ Console.WriteLine();   
+ }*/
+
+//Метод для заполнения и печати матрицы
+/*
+
+void PrintArray(int[,] matr)
+{
+   for (int i = 0; i < matr.GetLength(0); i++)
+   {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+       Console.Write($"{matr[i, j]} ");
+    }
+ Console.WriteLine();   
+ }
+}
+
+void FillArray(int[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        matr[i,j] = new Random().Next(1,10);
+    }
+  }
+}
+
+
+int[,] matrix = new int[3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+*/
+
+// задача с рисунком палец
+// int[,] pic = new int[23,25]     0-закраш пиксель, 1 - не закрашеный
+/*
+int[,] pic = new int[,] 
+{ 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 }, 
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, 
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, 
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 }, 
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, 
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, 
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 }, 
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 }, 
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 }, 
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 }, 
+{0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 }, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+
+void PrintImage(int[,] image)
+{
+   for (int i = 0; i < image.GetLength(0); i++)
+   {
+    for (int j = 0; j < image.GetLength(1); j++)
+    {
+       //Console.Write($"{image[i, j]} ");
+       if (image[i,j] == 0) Console.Write($" ");
+       else Console.Write($"+");
+    }
+ Console.WriteLine();   
+ }
+}
+// метод для закрашивания
+
+void FillImage(int row, int col)
+{
+  if (pic[row,col]==0)
+  {
+    pic[row, col] = 1;
+    FillImage(row-1, col);
+    FillImage(row, col-1);
+    FillImage(row+1, col);
+    FillImage(row, col+1);
+  }
+}
+
+PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic);
+*/
+
+// Рекурсия
+//задача вычисления факториала
+// 5! = 5*4*3*2*1
+/*
+double Factorial (int n)
+{
+if (n==1) return 1;
+else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+*/
+
+// Задача с Фибаначи f(1)=1 f(2)=1 f(n)= f(n-1)+f(n-2)
+
+double Fibonacci(int n)
+{
+  if (n==1 || n==2) return 1;
+  else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
